904b5a5fac8cc684011a51c91673464833403828
emin350 <eminar@hotmail.co.uk>
2022-02-20 02:24:59 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+import Country from "./components/Country";
+import Loading from "./components/Loading";
+import {CountryType} from "./types";
+
+function App() {
+ const [countries, setCountries] = useState<CountryType[]>([]);
+ const [loading, setLoading] = useState<boolean>(false);
+
+const getCountries = async () => {
+  setLoading(true);
+    try {
+const { data } = await axios.get<CountryType[]>("https://restcountries.com/v2/all");
+setCountries(data);
+    } catch {
+        console.log("hata oluştu")
+    } finally {
+      setLoading(false);
+    }
+  };
+
+useEffect(() => {
+
+getCountries();
+
+}, [])
+
+console.log({countries});
+
+  return (
+    <div>
+      <Loading loading={loading}> 
+        {countries.map((country) => {
+           return(
+          <Country key={country.name} country={country}/>
+              );
+        })}
+      </Loading>
+    </div>
+  )
+}
+
+export default App
